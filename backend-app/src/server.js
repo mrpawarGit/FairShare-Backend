5361c4eb125bb6d67bd18b46f41b0247ef7d2715
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-12-06 13:02:38 +0530

    Initial setup

--- backend-app/src/server.js
+++ backend-app/src/server.js
@@ -4,5 +4,5 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`FairShare Backend running on http://localhost:${PORT}`);
 });
