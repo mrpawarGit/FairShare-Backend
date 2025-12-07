b299e2b5fcc85e0a65a53951a07b79f17c1eaf8a
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-12-07 21:30:53 +0530

    updated

--- backend-app/src/config/firebase.js
+++ backend-app/src/config/firebase.js
@@ -4,7 +4,6 @@ const admin = require("firebase-admin");
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  // important: replace escaped \n with real newlines
   privateKey: process.env.FIREBASE_PRIVATE_KEY
     ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
     : undefined,
