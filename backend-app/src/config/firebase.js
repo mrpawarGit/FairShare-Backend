26c0a37a844d4b9ddeb1d3bc0f6ae7612c283e9e
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2025-12-07 21:30:34 +0530

    updated

--- backend-app/src/config/firebase.js
+++ backend-app/src/config/firebase.js
@@ -1,5 +1,14 @@
+// src/config/firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("../../fairshare-backend-firebase-adminsdk-fbsvc-e6bb7d7196.json");
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  // important: replace escaped \n with real newlines
+  privateKey: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : undefined,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
